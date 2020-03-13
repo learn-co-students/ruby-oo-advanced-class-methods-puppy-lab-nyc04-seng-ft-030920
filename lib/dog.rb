cfc3c006efd8f9603bb0368abc9d7dc356b92519
replacem3nts <donny.landis@gmail.com>
2020-03-12 22:45:09 -0400

    updates file to pass tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog
+attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        name_array = self.all.map {|dog| dog.name}
+        name_array.each {|dog_name| puts dog_name}
+    end
+end
